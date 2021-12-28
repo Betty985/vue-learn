bafda3b92014d7efcdd867f5f806ec83088b1192
Betty985 <1830265753@qq.com>
2021-12-28 20:06:28 +0800

    todo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,129 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header @toggle-add-task="toggleAddTask" title="Task Tracker"></Header>
+    <div v-show="showAddTask">
+    <AddTask @add-task="addTask" :showAddTask="showAddTask"/>
+    </div>
+    <Tasks :tasks="tasks" @delete-task="deleteTask" @toggle-reminder="toggleReminder"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Tasks,
+    AddTask
+  },
+  data () {
+    return {
+      tasks: [],
+      showAddTask: false
+    }
+  },
+  methods: {
+    toggleAddTask () {
+      this.showAddTask = !this.showAddTask
+    },
+    addTask (task) {
+      this.tasks = [...this.tasks, task]
+    },
+    deleteTask (id) {
+      if (confirm('are you sure?')) {
+        this.tasks = this.tasks.filter((task) => task.id !== id)
+      }
+    },
+    toggleReminder (id) {
+      this.tasks = this.tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task)
+    }
+  },
+  created () {
+    this.tasks = [
+      {
+        id: 1,
+        text: '5000字论文',
+        day: '2021.1.7',
+        reminder: true
+      },
+      {
+        id: 2,
+        text: '搜索引擎',
+        day: '2021.1.4',
+        reminder: true
+      },
+      {
+        id: 3,
+        text: '案例分析',
+        day: '2021.1.1',
+        reminder: true
+      },
+      {
+        id: 4,
+        text: 'xml实验设计',
+        day: '2021.1.6',
+        reminder: false
+      },
+      {
+        id: 5,
+        text: 'web站点',
+        day: '2021.1.9',
+        reminder: false
+      }
+    ]
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* @import url('https://fonts.googleapis.com/css2?family=Ponppins:wght@300;400&display=swap'); */
+
+/* 全局样式 */
+*{
+  box-sizing: border-box;
+  margin:0;
+  padding:0;
+}
+body {
+  font-family: 'Poppins',sans-serif;
+}
+.container{
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border:1px solid steelblue;
+  padding:30px;
+  border-radius: 5px;
+}
+
+.btn{
+  display: inline-block;
+  background: #000;
+  color:#fff;
+  border:none;
+  padding:10px 20px;
+  margin:5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+
+.btn:focus{
+  outline: none;
+}
+
+.btn:active{
+  transform: scale(0.98);
+}
+.btn-block{
+  display: block;
+  width:100%;
 }
 </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,55 @@
+<template>
+  <div :class="[task.reminder?'reminder':'','task']" @dblclick="$emit('toggle-reminder',task.id)">
+    <h3>
+      {{ task.text}}
+      <i @click="onDelete(task.id)" class="fas fa-times">x</i>
+    </h3>
+    <p>{{task.day}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  props: {
+    task: Object
+  },
+  methods: {
+    onDelete (id) {
+      this.$emit('delete-task', id)
+    }
+  }
+}
+</script>
+
+<style scope>
+.fas {
+  color: red;
+  font-style: normal;
+  /* font-family: "Font Awesome 5 Free";
+  font-weight: 900;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  display: inline-block;
+  font-variant: normal;
+  text-rendering: auto; */
+  line-height:1
+}
+/* .fa-times:before {
+    content: "\f00d"
+} */
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left:5px solid green;
+}
+.task h3{
+  display: flex;
+  align-items: center;
+  justify-content:space-between;
+}
+</style>
